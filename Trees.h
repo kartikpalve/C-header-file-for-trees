9f5592743db4ece6fd5a09e9a8ab0a8edec3216a
Kartik <palve.kartik101004@gmail.com>
2023-10-31 18:57:26 +0530

    First commit

---
+++ Trees.h
@@ -0,0 +1,12 @@
+#ifndef Trees_h
+#define Trees_h
+
+#include "binaryTree_int.h"
+#include "binaryTree_float.h"
+#include "binaryTree_char.h"
+#include "binaryTree_string.h"
+#include "binarySearchTree_int.h"
+#include "binarySearchTree_char.h"
+#include "binarySearchTree_float.h"
+
+#endif /*Trees_h*/
